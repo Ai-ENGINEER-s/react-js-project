492dbea5c3f50a51478327a32fafea7d8f9edac8
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-19 14:19:37 +0100

    react project

--- project_1_info_site/project_1_info_site/src/App.test.js
+++ project_1_info_site/project_1_info_site/src/App.test.js
@@ -6,3 +6,27 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
