0df613566b86123b4e5c35f8ff2ce5060d3726e9
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-21 08:43:25 +0100

    react projects

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -1,69 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
+import { useState,useEffect} from 'react';
+
+// Props dans React = > properties in React
 
 
-// Props dans React = > properties in React 
+// function useWindowWidth() {
+//   const [width, setWidth] = useState(window.innerWidth);
 
-const productList = [
-  {
-    name:"fanta" ,
-    price : 200
+//   useEffect(() => {
+//     const handleResize = () => setWidth(window.innerWidth);
+//     window.addEventListener('resize', handleResize);
 
-  },
-  {
-    name:"habit" ,
-    price : 2000
-    
-  }, 
-  {
-    name:"Pantalon" ,
-    price : 20000
-    
-  }, 
-  {
-    name:"Montre" ,
-    price : 200
-    
-  }
-]
+//     return () => {
+//       window.removeEventListener('resize', handleResize);
+//     };
+//   }, []);
+
+//   return width;
+// }
+
+// function MyComponent() {
+//   const width = useWindowWidth();
+
+//   return <div>La largeur de la fenêtre est : {width}px</div>;
+// }
 
 
+// function App(){
+// return (
+
+//   <div>
+// <MyComponent/>
+
+//   </div>
+// )
 
 
-
-function MyListOfProduct(){
-
-return (
-
-productList.map((item , index  )=>{
-
-return (
-  <ul className='product'>
-   
-   <li key={index}>
-          <strong>{item.name}</strong>: {item.price} FCFA
-        </li>
-
-  </ul>
-)
-})
-)
+// }
 
 
+// export default App 
 
 
-}
+// useState =>  
+
+// le hook useState permet de gerer l'etat dans un composant focntionnel . il initialise une valeur  d'etat et retourne un tableau conetenant la valeur actuelle et une fonction pour la mettre a jour 
 
 
- export default function App (){
+const [something , setSomething] = useState(setSomething)
+
+function Counter(){
+
+  const [myConuntNumber , setMyCounteNumber] = useState(0);
 
   return (
+
     <div>
-    <MyListOfProduct/>
+      Vous avez cliqué {myConuntNumber} fois 
     </div>
+
+    <button onClick={()=>set}></button>
   )
 }
-
-
-
