105b0f11133c0e5501ee17bc4c26be752b3b9604
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-22 14:38:07 +0100

    tic tac toe game

--- project_2_tic_tac_toe_game/src/App.js
+++ project_2_tic_tac_toe_game/src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+
+
+
+
+
+function Square(props){
+
+  return <button className='square'>{props.name}</button>
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='squareContainer'>
+
+
+  <Square name="0"/>
+  <Square name="1"/>
+  <Square name="1"/>
+  <Square name="1"/>
+  <Square name="0"/>
+  <Square name="0"/>
+  <Square name="1"/>
+  <Square name="0"/>
+  <Square name="0"/>
+</div>
   );
 }
 
