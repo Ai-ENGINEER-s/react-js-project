42b76a9f3a9c7a7a464e1dcb1e3cb0923940b5c8
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-27 22:16:27 +0100

    test appplication tic tae toe game

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -5,14 +5,22 @@ import { useState,useEffect, useCallback, useContext, useReducer, createContext}
 
 
 
-function Square({value}){
+function Square(){
+
+  const [value , setValue] = useState(null)
+
+
 
   function handleClick(){
-    
+      setValue('X')
   }
 
   return (
-   <button className='square'>{value}</button>
+   <button className='square' onClick={handleClick} 
+    >{value}
+    
+    </button>
+   
   )
 }
 
@@ -23,27 +31,26 @@ function Board(){
   <>
 
   <div className='board-row'>
+      <Square />
+      <Square />
+      <Square />
 
-      <Square value="1"/>
-      <Square value="2"/>
-      <Square value="3"/>
-
-
-  </div>
-
-  <div className='board-row'>
-
-      <Square value="4"/>
-      <Square value="5"/>
-      <Square value="6"/>
 
   </div>
 
   <div className='board-row'>
 
-      <Square value="7"/>
-      <Square value="8"/>
-      <Square value="9"/>
+  <Square />
+  <Square />
+  <Square />
+
+  </div>
+
+  <div className='board-row'>
+
+  <Square />
+  <Square />
+  <Square />
 
   </div>
 
