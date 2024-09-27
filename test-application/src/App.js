0349c28843b8fcb0e8f1f4f97bf805e1877b69c4
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-27 15:33:39 +0100

    test application tic tac toe game

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -5,6 +5,56 @@ import { useState,useEffect, useCallback, useContext, useReducer, createContext}
 
 
 
+function Square({value}){
+
+  function handleClick(){
+    
+  }
+
+  return (
+   <button className='square'>{value}</button>
+  )
+}
+
+function Board(){
+
+  return (
+
+  <>
+
+  <div className='board-row'>
+
+      <Square value="1"/>
+      <Square value="2"/>
+      <Square value="3"/>
+
+
+  </div>
+
+  <div className='board-row'>
+
+      <Square value="4"/>
+      <Square value="5"/>
+      <Square value="6"/>
+
+  </div>
+
+  <div className='board-row'>
+
+      <Square value="7"/>
+      <Square value="8"/>
+      <Square value="9"/>
+
+  </div>
+
+    </>
+  )
+}
+
+
+
+
+
 
 
 export default function  App(){
@@ -13,11 +63,8 @@ export default function  App(){
 
 
     <div>
-    <h1>
-      welcome to your beautiful game tic tac toe game <br></br> 
-
-      
-    </h1>
+ 
+    <Board></Board>
     </div>
   )
 }
