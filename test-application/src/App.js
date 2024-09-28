57248ed06c57e018bb34b0c681fd3702918d5a0c
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-09-28 16:22:40 +0100

    tic toe game

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -5,11 +5,11 @@ import { useState,useEffect, useCallback, useContext, useReducer, createContext}
 
 
 
-function Square({value , onClickSquare}){
+function Square({value ,onSquareClick}){
 
   return (
 
-    <button className='squarre' onClick={onClickSquare}>{value}
+    <button className='squarre' onClick={onSquareClick}>{value}
 
     </button>
   )
@@ -19,28 +19,101 @@ function Square({value , onClickSquare}){
 function Board(){
 
   const [squares , setSquares] = useState(Array(9).fill(null))
-
+  const [xIsNext , setXisNext] = useState(true);
   
-
-  function handleClick(){
-
-    const nextSquares = squares.slice()
-    nextSquares[0] = "X"
-  
-    setSquares(nextSquares)
+function handleClick(i){
+  if(calculateWinner(squares) || squares[i]){
+    return ;
   }
+
+  
+    const nextSquares = squares.slice()
+
+    if(xIsNext){
+      nextSquares[i] = "X";
+
+    }else{
+
+      nextSquares[i] = "O";
+    }
+    setSquares(nextSquares);
+    setXisNext(!xIsNext)
+    
+  
+  }
+
+
+  const winner = calculateWinner(squares); 
+  let status ; 
+
+  if(winner){
+
+    status = winner +'a gagné' ; 
+  }else {
+
+    status = 'Prochain tour : ' + (xIsNext ?  'X' : 'O'); 
+  }
+  
+    
+  
 
 return (
 
-  <div>
-
-    <Square value={squares[0]} onClickSquare={handleClick}/>
-  </div>
+<>
+<h1>{status}</h1>
+<div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+</>
 )
 
 
 }
 
+// create victory function to show wich user gonna win the game 
+
+
+
+function calculateWinner(squares){
+
+const lines = [
+
+[0,1,2], 
+[3,4,5], 
+[6,7,8], 
+[0,3,6], 
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6],
+
+
+
+];
+
+for (let i =0;i<lines.length; i++){
+
+  const [a , b ,c ]= lines[i]; 
+
+  if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+
+    return squares[a]
+  }
+}
+return null ; 
+}
 function App(){
 
 return (
