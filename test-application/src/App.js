ece6400204be2411efbc50ababb7046f8de397ca
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-10-02 21:57:02 +0100

    chnage background color

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -22,36 +22,84 @@ function Square({value, onClickSquare}){
 
 function Board(){
 
+
+
+
   const [squares , setSquares] = useState(Array(9).fill(null))
 
   const [checkSquare , setCheckSquare] = useState(true)
 
-
+  let  champion ; 
 
   const nextSquares = squares.slice()
-  function handleClick(i){
+  
+  
+  // handleclick  function  for clicking button 
+
+
+function handleClick(i){
+
+
+const gameWinner = winner(squares)
 
 
 
-if(nextSquares[i]) return ;
 
+if(nextSquares[i] || gameWinner) return ;
+
+if(gameWinner){
+  champion = gameWinner + "vous avez gagné"
+}
 nextSquares[i] = checkSquare ? "X" : "O"
-
+ 
 setCheckSquare(!checkSquare)
 setSquares(nextSquares)
 
   }
+
+function winner(squares){
+
+const arrays = [ 
+
+
+  [0,1,2] , 
+  [3,4,5], 
+  [6,7,8],
+   [0,3,6], 
+   [1,4,7], [2,5,8] , [0,4,8], [2,4,6]
+]
+
+for (let i = 0; i<arrays.length ; i++){
+
+let a ,b,c ;
+ a = arrays[i][0]
+ b = arrays[i][1]
+ c = arrays[i][2]
+
+
+
+if(squares[a]&&squares[a] === squares[b] && squares[b] === squares[c]){
+  return squares[a] ; 
+}
+}
+return null ;
+
+
+}
+
+
   return (
 
-    <>
-    <div>
+   <div>
+  <h3>{champion}</h3>  
+<div>
 
-      <Square  value={squares[0]}  onClickSquare={()=>{handleClick(0)}}/>
-      <Square  value={squares[1]}  onClickSquare={()=>{handleClick(1)}}/>
-      <Square  value={squares[2]}  onClickSquare={()=>{handleClick(2)}}/>
-    </div>
-    
-    <div>
+<Square  value={squares[0]}  onClickSquare={()=>{handleClick(0)}}/>
+<Square  value={squares[1]}  onClickSquare={()=>{handleClick(1)}}/>
+<Square  value={squares[2]}  onClickSquare={()=>{handleClick(2)}}/>
+</div>
+
+<div>
 
 <Square  value={squares[3]}  onClickSquare={()=>{handleClick(3)}}/>
 <Square  value={squares[4]}  onClickSquare={()=>{handleClick(4)}}/>
@@ -65,9 +113,14 @@ setSquares(nextSquares)
 <Square  value={squares[8]}  onClickSquare={()=>{handleClick(8)}}/>
 </div>
 
-    </>
+
+   </div>
   )
 }
+
+
+
+
 
 
 
