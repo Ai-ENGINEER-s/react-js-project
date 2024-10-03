5713836f9736c8e74dd3e832c22a9a97b1fa6d97
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-10-03 09:10:16 +0100

    tic tac toe game

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -1,142 +1,84 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
-import { useState} from 'react';
 
-
-
-function Square({value, onClickSquare}){
-
-
+function Square({ value, onClickSquare }) {
   return (
-
-
-    <button className='square'  onClick={onClickSquare}>
-
-{value}
+    <button className='square' onClick={onClickSquare}>
+      {value}
     </button>
-  )
+  );
 }
 
+function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [checkSquare, setCheckSquare] = useState(true);
+  const [champion, setChampion] = useState('Next player: X');
 
+  function handleClick(i) {
+    if (squares[i] || winner(squares)) return;
 
-function Board(){
+    const nextSquares = squares.slice();
+    nextSquares[i] = checkSquare ? 'X' : 'O';
+    setSquares(nextSquares);
 
+    const gameWinner = winner(nextSquares);
+    if (gameWinner) {
+      setChampion(gameWinner + ' a gagné');
+    } else {
+      setChampion('Next player: ' + (checkSquare ? 'O' : 'X'));
+    }
 
-
-
-  const [squares , setSquares] = useState(Array(9).fill(null))
-
-  const [checkSquare , setCheckSquare] = useState(true)
-
-
-
-  const nextSquares = squares.slice()
-  let  [champion , setChampion] = useState(null)
-  
-  // handleclick  function  for clicking button 
-
-
-function handleClick(i){
-
-
-const gameWinner = winner(nextSquares)
-
-
-
-
-if(nextSquares[i] || gameWinner) return ;
-
-if(gameWinner){
-  champion = gameWinner + "vous avez gagné"
-}else{
-  champion ="Personne n'a gagné au jeux"
-}
-nextSquares[i] = checkSquare ? "X" : "O"
- 
-setCheckSquare(!checkSquare)
-setSquares(nextSquares)
-setChampion(champion)
-
+    setCheckSquare(!checkSquare);
   }
 
-function winner(squares){
+  function winner(squares) {
+    const winPatterns = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-const arrays = [ 
-
-
-  [0,1,2] , 
-  [3,4,5], 
-  [6,7,8],
-   [0,3,6], 
-   [1,4,7], [2,5,8] , [0,4,8], [2,4,6]
-]
-
-for (let i = 0; i<arrays.length ; i++){
-
-let a ,b,c ;
- a = arrays[i][0]
- b = arrays[i][1]
- c = arrays[i][2]
-
-
-
-if(squares[a]&&squares[a] === squares[b] && squares[b] === squares[c]){
-  return squares[a] ; 
-}
-}
-return null ;
-
-
-}
-
+    for (let [a, b, c] of winPatterns) {
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
 
   return (
-
-   <div>
-  <h3>{champion}</h3>  
-<div>
-
-<Square  value={squares[0]}  onClickSquare={()=>{handleClick(0)}}/>
-<Square  value={squares[1]}  onClickSquare={()=>{handleClick(1)}}/>
-<Square  value={squares[2]}  onClickSquare={()=>{handleClick(2)}}/>
-</div>
-
-<div>
-
-<Square  value={squares[3]}  onClickSquare={()=>{handleClick(3)}}/>
-<Square  value={squares[4]}  onClickSquare={()=>{handleClick(4)}}/>
-<Square  value={squares[5]}  onClickSquare={()=>{handleClick(5)}}/>
-</div>
-
-<div>
-
-<Square  value={squares[6]}  onClickSquare={()=>{handleClick(6)}}/>
-<Square  value={squares[7]}  onClickSquare={()=>{handleClick(7)}}/>
-<Square  value={squares[8]}  onClickSquare={()=>{handleClick(8)}}/>
-</div>
-
-
-   </div>
-  )
+    <div>
+      <h3>{champion}</h3>
+      <div>
+        <Square value={squares[0]} onClickSquare={() => handleClick(0)} />
+        <Square value={squares[1]} onClickSquare={() => handleClick(1)} />
+        <Square value={squares[2]} onClickSquare={() => handleClick(2)} />
+      </div>
+      <div>
+        <Square value={squares[3]} onClickSquare={() => handleClick(3)} />
+        <Square value={squares[4]} onClickSquare={() => handleClick(4)} />
+        <Square value={squares[5]} onClickSquare={() => handleClick(5)} />
+      </div>
+      <div>
+        <Square value={squares[6]} onClickSquare={() => handleClick(6)} />
+        <Square value={squares[7]} onClickSquare={() => handleClick(7)} />
+        <Square value={squares[8]} onClickSquare={() => handleClick(8)} />
+      </div>
+    </div>
+  );
 }
 
-
-
-
-
-
-
-function App(){
-
-
-  return <>
-  
-  <Board></Board>
-  
-  </>
+function App() {
+  return (
+    <>
+      <Board />
+    </>
+  );
 }
 
-
-export default App 
-
+export default App;
