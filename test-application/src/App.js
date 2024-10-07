6a58cf82920babcf6ed9bbefd58fecf7ab682a43
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-10-07 08:34:09 +0100

    tic tac toe game

--- test-application/src/App.js
+++ test-application/src/App.js
@@ -16,11 +16,17 @@ function Square({value , onclickSquarre}){
 
 function calculateWinner(squares){
 
+const boardSquares = [
+[0,1,2], [3,4,5] , [6,7,8], 
+[0,3,6],[1,4,7], [2,5,8],[0,4,8],[2,4,6]
+
+]
+
+for (let i =0; i<boardSquares.length ; i++){
+
+
   
-
-
-
-
+}
 
 }
 
