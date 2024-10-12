717a31c51d1bf0445d42f12d6aaba8922481ab04
Ai-ENGINEER-s <barrysanoussa19@gmail.com>
2024-10-12 20:09:00 +0100

    movieLand

--- Project_3_MovieLand_app/src/App.js
+++ Project_3_MovieLand_app/src/App.js
@@ -4,39 +4,14 @@ import {useState , useEffect } from 'react' ;
 
 
 
-const Counter = ()=>{
-
-
-  const [counter , setCounter] = useState(0) ;
-  
-  useEffect(()=>{
- 
-function handleCouncter(){
-
-  return (
-    <h2>BARRRY SAONUSSA</h2>
-  )
-}
-
-
-handleCouncter()
-      } , [counter])
-  return (
-
-  <>
-    <button onClick={()=>{setCounter(counter +1)}}>+</button>
-    <h1>{counter}</h1>
-    <button onClick={()=>{setCounter(counter -1)}}>-</button>
-  </>
-  )
-}
 
 
  const  App =  ()=>{
 
 
+
   return (
-<Counter/>
+<button >BARRY</button>
   )
 
 }
